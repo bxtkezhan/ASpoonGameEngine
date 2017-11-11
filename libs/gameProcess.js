0e699e817bc9bbc698bf91a2c63d1c4df9d36baa
bxtkezhan <bxtkezhan@qq.com>
2017-11-11 14:57:09 +0800

    优化 game process

--- libs/gameProcess.js
+++ libs/gameProcess.js
@@ -1,27 +1,23 @@
-window.requestAnimationFrame =  window.requestAnimationFrame ||
-                                window.webkitRequestAnimationFrame ||
-                                window.mozRequestAnimationFrame ||
-                                window.oRequestAnimationFrame ||
-                                window.msRequestAnimationFrame;
-
 function GameProcess(_ready=null, _process=null) {
+  var requestAnimationFrame = window.requestAnimationFrame ||
+                              window.webkitRequestAnimationFrame ||
+                              window.mozRequestAnimationFrame ||
+                              window.oRequestAnimationFrame ||
+                              window.msRequestAnimationFrame;
   var last_timestamp = null;
-  function system_ready() {
-    if (_ready != null) _ready();
-    if (_process != null) {
-      window.requestAnimationFrame(system_process);
-    }
-  }
 
   function system_process(timestamp) {
     if (last_timestamp != null) {
-      _process((timestamp - last_timestamp) * 0.01);
+      _process((timestamp - last_timestamp) * 0.001);
     }
     last_timestamp = timestamp;
-    window.requestAnimationFrame(system_process);
+    requestAnimationFrame(system_process);
   }
 
   this.start = function () {
-    system_ready();
+    if (_ready != null) _ready();
+    if (_process != null) {
+      system_process();
+    }
   }
 }
