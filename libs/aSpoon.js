40c928ea8ce9602b508f15aa7225828e717cfe18
bxtkezhan <bxtkezhan@qq.com>
2017-11-12 12:52:43 +0800

    完善游戏进程结构，添加stop功能

--- libs/aSpoon.js
+++ libs/aSpoon.js
@@ -1,23 +1,50 @@
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          window.oRequestAnimationFrame      ||
+          window.msRequestAnimationFrame     ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
 function asge_process(_ready=null, _process=null) {
-  var requestAnimationFrame = window.requestAnimationFrame ||
-                              window.webkitRequestAnimationFrame ||
-                              window.mozRequestAnimationFrame ||
-                              window.oRequestAnimationFrame ||
-                              window.msRequestAnimationFrame;
-  var last_timestamp = null;
+  asge_process_obj = {
+    _ready: _ready,
+    _process: _process,
+    last_timestamp: null,
+    is_actived: false,
 
-  function system_process(timestamp) {
-    if (last_timestamp != null) {
-      _process((timestamp - last_timestamp) * 0.001);
-    }
-    last_timestamp = timestamp;
-    requestAnimationFrame(system_process);
+    system_process (timestamp) {
+      if (this.is_actived) {
+        if (this.last_timestamp != null) {
+          this._process((timestamp - this.last_timestamp) * 0.001);
+        }
+        this.last_timestamp = timestamp;
+        requestAnimationFrame(this.system_process.bind(this));
+      }
+    },
+
+    start (use_ready=true) {
+      if (this._ready != null && use_ready) this._ready();
+      if (this._process != null) {
+        this.last_timestamp = null;
+        this.is_actived = true;
+        this.system_process();
+      }
+    },
+
+    stop() {
+      this.is_actived = false;
+    },
+
+    getActive() {
+      return this.is_actived;
+    },
   }
 
-  this.start = function () {
-    if (_ready != null) _ready();
-    if (_process != null) system_process();
-  }
+  return asge_process_obj;
 }
 
 var ASGE = {
