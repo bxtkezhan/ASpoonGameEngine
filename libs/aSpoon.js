ce1cbafe5edb20dd38d5d687055c37cd318c0433
bxtkezhan <bxtkezhan@qq.com>
2017-11-17 17:57:25 +0800

    添加scene，camera，renderer到Process

--- libs/aSpoon.js
+++ libs/aSpoon.js
@@ -1,4 +1,4 @@
-window.requestAnimFrame = (function(){
+window.requestAnimFrame = new function (){
   return  window.requestAnimationFrame       ||
           window.webkitRequestAnimationFrame ||
           window.mozRequestAnimationFrame    ||
@@ -7,7 +7,7 @@ window.requestAnimFrame = (function(){
           function( callback ){
             window.setTimeout(callback, 1000 / 60);
           };
-})();
+};
 
 function asge_process(_ready=null, _process=null) {
   asge_process_obj = {
@@ -16,6 +16,16 @@ function asge_process(_ready=null, _process=null) {
     last_timestamp: null,
     is_actived: false,
 
+    _scene: new THREE.Scene(),
+    _camera: new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100),
+    _renderer: new function () {
+      var renderer = new THREE.WebGLRenderer();
+      renderer.setClearColor(new THREE.Color(0xffffff));
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.shadowMap.enabled = true;
+      return renderer;
+    },
+
     system_process (timestamp) {
       if (this.is_actived) {
         if (this.last_timestamp != null) {
@@ -23,11 +33,14 @@ function asge_process(_ready=null, _process=null) {
         }
         this.last_timestamp = timestamp;
         requestAnimationFrame(this.system_process.bind(this));
+        this._renderer.render(this._scene, this._camera);
       }
     },
 
     start (use_ready=true) {
-      if (this._ready != null && use_ready) this._ready();
+      if (this._ready != null && use_ready) {
+        this._ready(this._scene, this._camera, this._renderer);
+      }
       if (this._process != null) {
         this.last_timestamp = null;
         this.is_actived = true;
